64ab1d1ad43755d9882dfe18a15af1df12c5953e
cdfortes <cdfortes@gmail.com>
2022-06-21 17:04:28 -0100

    Done

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  genreSelected: number;
+  handleClickButton: (id: number) => void;
+}
+
+export function SideBar({ genres, genreSelected, handleClickButton }: SideBarProps) {
+  return (
+    <nav className='sidebar'>
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className='buttons-container'>
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={genreSelected === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
